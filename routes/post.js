ada2566f868863700e316fae8636532d3c95458a
hannahganne <ganne.hannah@gmail.com>
2022-06-30 20:50:53 +0200

    remove unused code

--- routes/post.js
+++ routes/post.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
-const postCtrl = require('../controllers/Post.js');
+const postCtrl = require('../controllers/Post');
+const userCtrl = require('../controllers/User')
 
 router.post('/', auth, multer, postCtrl.createPost);
 router.get('/', auth, postCtrl.getAllPosts);
@@ -10,6 +11,7 @@ router.get('/:id', auth, postCtrl.getPost);
 router.put('/:id', auth, multer, postCtrl.modifyPost);
 router.delete('/:id', auth, postCtrl.deletePost);
 router.post('/:id/like', auth, postCtrl.likePost);
+router.get('/:id/viewProfile', auth, userCtrl.viewProfile);
 
 
 module.exports = router; 
