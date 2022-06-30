bbde28f2f76f1f6c179a854c74d762fb364ddf83
hannahganne <ganne.hannah@gmail.com>
2022-06-30 20:51:19 +0200

    add setProfile route

--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const limiter = require('../middleware/rateLimiter');
 const passwordValidator = require('../middleware/password-validator');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 const userCtrl = require('../controllers/User.js');
 
 router.post('/signup', passwordValidator, userCtrl.signup);
 router.post('/login', limiter.loginLimiter, userCtrl.login);
+router.put('/setProfile', auth, multer, userCtrl.setProfile);
+
 
 module.exports = router; 
