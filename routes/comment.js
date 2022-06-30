cb8b8e9f0fed4962ffeaf35ce6c9b483bcc31d26
hannahganne <ganne.hannah@gmail.com>
2022-06-30 20:50:24 +0200

    edit comment routes

--- routes/comment.js
+++ routes/comment.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const commentCtrl = require('../controllers/Comment.js');
 
-router.post('/', auth, commentCtrl.createComment);
-router.get('/', auth, commentCtrl.getAllComments);
+router.post('/:id/comment', auth, commentCtrl.createComment);
+router.get('/:id/comment', auth, commentCtrl.getAllComments);
 
 module.exports = router; 
