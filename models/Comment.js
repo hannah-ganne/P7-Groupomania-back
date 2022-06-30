2434d8b21149b3ef8cbda4ce247740b7e0ee856d
hannahganne <ganne.hannah@gmail.com>
2022-06-30 20:49:02 +0200

    remove unused code

--- models/Comment.js
+++ models/Comment.js
@@ -1,13 +1,10 @@
 // import sequelize, init DataTypes
 const { Sequelize, DataTypes } = require('sequelize');
-
-// import connection
 const db = require('../config/database.js');
 
 // define schema
 const Comment = db.define('comment', {
     // define attributes
-    userId: { type: DataTypes.STRING, allowNull: false },
     comment: { type: DataTypes.TEXT, allowNull: false },
 }, {
     freezeTableName: true
