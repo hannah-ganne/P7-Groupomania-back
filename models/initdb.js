d920c5a63c1cc7500cebd196c2672eb8fdc7a704
hannahganne <ganne.hannah@gmail.com>
2022-07-07 12:17:16 +0200

    add sequelize associations

--- models/initdb.js
+++ models/initdb.js
@@ -3,18 +3,27 @@ const User = require('./user');
 const Post = require('./post');
 const Comment = require('./comment');
 const Like = require('./likes');
+// const Populate = require('./populate');
+const Department = require('./department');
+const Topic = require('./topic')
 
 let Init = async () => {
     Post.belongsTo(User, { onDelete: "CASCADE" });
-    Comment.belongsTo(User);
-    Comment.belongsTo(Post);
-    Like.belongsTo(Post);
-    Like.belongsTo(User);
+    Post.hasMany(Comment);
+    Comment.belongsTo(User, { onDelete: "CASCADE" });
+    Comment.belongsTo(Post, { onDelete: "CASCADE" });
+    Post.hasMany(Like);
+    Like.belongsTo(Post, { onDelete: "CASCADE" });
+    Like.belongsTo(User, { onDelete: "CASCADE" });
 
     await User.sync({alter: true});
     await Post.sync({alter: true});
     await Comment.sync({ alter: true });
     await Like.sync({ alter: true });
+    await Department.sync({ alter: true });
+    await Topic.sync({ alter: true });
+
+    // await Populate();
 }
 
 module.exports = Init;
