55257dffc612a5c75e0feceb907e5938bc0a33e9
lanomade <ganne.hannah@gmail.com>
2022-08-07 21:20:49 +0200

    add deleteComment route

--- routes/comment.js
+++ routes/comment.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const commentCtrl = require('../controllers/Comment.js');
 
-router.post('/:id/comment', auth, commentCtrl.createComment);
+router.post('/:id/comment/', auth, commentCtrl.createComment);
+router.delete('/:id/comment/:commentId', auth, commentCtrl.deleteComment);
 
 module.exports = router; 
