edc83da9d42ef24fe8879745440dd642cc53249f
lanomade <ganne.hannah@gmail.com>
2022-08-07 21:20:24 +0200

    delete allowNull clause for imageUrl

--- models/post.js
+++ models/post.js
@@ -10,7 +10,7 @@ const Post = db.define('post', {
     title: { type: DataTypes.STRING, allowNull: false }, 
     topic: { type: DataTypes.STRING, allowNull: false },
     description: { type: DataTypes.TEXT, allowNull: false },
-    imageUrl: { type: DataTypes.STRING, allowNull: false},
+    imageUrl: { type: DataTypes.STRING },
 }, {
     freezeTableName: true
 });
