36cfd559f7a4ff42914bbc29c08482352b4716ad
lanomade <ganne.hannah@gmail.com>
2022-08-07 21:21:19 +0200

    edit post routes

--- routes/post.js
+++ routes/post.js
@@ -8,12 +8,14 @@ const postCtrl = require('../controllers/Post');
 
 router.post('/', auth, multer, postCtrl.createPost);
 router.get('/', auth, postCtrl.getAllPosts);
+router.get('/all/:sort', auth, postCtrl.sortPosts);
 router.get('/:id', auth, postCtrl.getPost);
 router.put('/:id', auth, multer, postCtrl.modifyPost);
 router.delete('/:id', auth, postCtrl.deletePost);
-router.get('/filter/department', auth, postCtrl.filterByDept);
-router.get('/filter/topic', auth, postCtrl.filterByTopic);
-router.get('/search/keyword', auth, postCtrl.searchPosts);
+router.get('/department/:department', auth, postCtrl.filterByDept);
+router.get('/topic/:topic', auth, postCtrl.filterByTopic);
+router.get('/search/:keyword', auth, postCtrl.searchPosts);
+router.get('/user/:userId', auth, postCtrl.getPostsByUser);
 
 // router.post('/:id/like', auth, postCtrl.likePost);
 // router.get('/:id/viewProfile', auth, userCtrl.viewProfile);
