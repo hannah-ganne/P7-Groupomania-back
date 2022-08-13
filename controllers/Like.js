0d079d15a657dc92f351fe6d27afa61c66ef02f0
lanomade <ganne.hannah@gmail.com>
2022-08-13 21:37:22 +0200

    refactor like controller

--- controllers/Like.js
+++ controllers/Like.js
@@ -5,38 +5,26 @@ const Like = require('../models/likes');
  */
 exports.likePost = (req, res, next) => {
     const likeObject = {
-        type: req.params.like,
+        type: req.body.like,
         postId: req.params.id,
         userId: req.token.userId
     }
 
-        switch (req.body.like) {
-            case 1:
-            case -1:
-                Like.findOne({ where: { postId: req.params.id, userId: req.token.userId } })
-                    .then(like => {
-                        if (!like) {
-                            Like.create(likeObject)
-                            .then(() => res.status(201).json({ message: 'like or disliked'}))
-                            .catch(error => res.status(400).json({ error }));
-                        } else {
-                            res.send({ message: 'Post already liked or disliked'})
-                        }
-                    })
-                    .catch(error => res.status(500).json({ message: "There's an " + error }));
-                break; 
-            
-            case 0:
-                Like.findOne({ where: { postId: req.params.id, userId: req.token.userId } })
-                    .then(like => {
-                        if (like) {
-                            Like.destroy({ where: { postId: req.params.id, userId: req.token.userId }})
-                            .then(() => res.status(200).json({ message: 'like or disliked cancelled'}))
-                            .catch(error => res.status(400).json({ error }));
-                        } else {
-                            res.send({ message: 'Post not rated yet'})
-                        }
-                    })
+    Like.findOne({ where: { postId: req.params.id, userId: req.token.userId } })
+        .then(like => {
+            if (!like) {
+                Like.create(likeObject)
+                .then(() => res.status(201).json({ message: 'like or disliked'}))
+                .catch(error => res.status(400).json({ error }));
+            } else {
+                if (req.body.like === like.type) {
+                    Like.destroy({ where: { postId: req.params.id, userId: req.token.userId }})
+                    .then(() => res.status(200).json({ message: 'like or disliked cancelled'}))
+                    .catch(error => res.status(400).json({ error }));
+                } else {
+                    res.send({ message: 'Post already liked or disliked'})
+                }
+            }
+        })
+        .catch(error => res.status(500).json({ message: "There's an " + error }));
         }
-
-    }
